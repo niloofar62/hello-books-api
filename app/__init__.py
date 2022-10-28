18ab73ed7e6cbde7b36639910d3380f0909355b2
Niloofar Nayebi <nil_nyb@yahoo.com>
2022-10-28 17:54:17 -0400

    set up done

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+#database object
+db=SQLAlchemy()
+migrate=Migrate()
+
+#postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    
+    db.init_app(app)
+    migrate.init_app(app,db)
+    from app.models.book import Book
 
     # Register Blueprints here
     from .routes import books_bp
---
+++ app/models/book.py
@@ -0,0 +1,8 @@
+from app import  db  
+class Book(db.Model) :
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    
+    def to_string(self):
+        return f"{self.id}",{self.title},{self.description}
